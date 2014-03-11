298451be97d14b5b6713a9eb06fc568ef9199f5f
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-11 11:17:10 +0000

    home template completed

--- tags/zestork/zestork/Content/app/beforeLoginApp.js
+++ tags/zestork/zestork/Content/app/beforeLoginApp.js
@@ -56,6 +56,10 @@ ZestorkApp.controller('beforeLoginHeaderController', function ($scope) {
 
 ZestorkApp.controller('homeTemplate', function ($scope) {
 
+    $scope.dataLoaded={
+        status:true
+    }
+
     $scope.sliderImageList = [
           { ImageAlt: "slider1", ImageUrl: "../../Resource/templates/beforeLogin/web/images/slider1.jpg" },
           { ImageAlt: "slider2", ImageUrl: "../../Resource/templates/beforeLogin/web/images/slider2.jpg" },
@@ -63,5 +67,60 @@ ZestorkApp.controller('homeTemplate', function ($scope) {
           { ImageAlt: "slider4", ImageUrl: "../../Resource/templates/beforeLogin/web/images/slider4.jpg" },          
         ];
 
+    $scope.gridIconDataList = [
+          { ImageLink: "../../Resource/templates/beforeLogin/web/about.html", 
+            ImageIconUrl: "../../Resource/templates/beforeLogin/web/images/g1.png",
+            Headline: "Success Stories",
+            Content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            BottomButtonUrl:"../../Resource/templates/beforeLogin/web/about.html",
+            BottomButtonText:"Read More"
+             },
+          { ImageLink: "../../Resource/templates/beforeLogin/web/services.html", 
+            ImageIconUrl: "../../Resource/templates/beforeLogin/web/images/g2.png",
+            Headline: "Custom Design",
+            Content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            BottomButtonUrl:"../../Resource/templates/beforeLogin/web/about.html",
+            BottomButtonText:"Read More"
+             },
+          { ImageLink: "../../Resource/templates/beforeLogin/web/services.html", 
+            ImageIconUrl: "../../Resource/templates/beforeLogin/web/images/g3.png",
+            Headline: "Customer Support",
+            Content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            BottomButtonUrl:"../../Resource/templates/beforeLogin/web/services.html",
+            BottomButtonText:"Read More"
+             }
+        ];
+
+        $scope.bottomContent = {
+
+            leftColumnHeading : "Our Vision",
+            leftColumnContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."+
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            leftColumnLink : "moreinfo.html",
+
+            middleColumnHeading : "What we do",
+            middleColumnContentList : [
+              { Content: "Lorem ipsum dolor sit amet qui officia" },
+              { Content: "Duis aute irure dolor in culpa qui" },
+              { Content: "Duis aute irure dolor in culpa qui2" },
+              { Content: "Duis aute irure dolor in culpa qui3" },
+              { Content: "Sunt in culpa qui officia vel illum qui"},
+              { Content: "Sunt in culpa qui officia vel illum qui2" },
+              { Content: "vel illum qui dolorem eum wise man therefore" },
+              { Content: "vel illum qui dolorem eum wise man therefore" }
+        ],
+            rightColumnHeading :"Our Stafff",
+            rightColumnImageList: [
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-1.jpg" },
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-2.jpg" },
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-3.jpg" },
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-4.jpg" },
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-5.jpg"},
+              { imgUrl: "../../Resource/templates/beforeLogin/web/images/management-6.jpg" },              
+        ],
+            rightColumnLink:"viewall.html"
+            
+        //copyRightInfo: "<p>Company Name © All Rights Reseverd | Design by  <a href=\"http://zestork.com\">W3Layouts</a></p>"        
+    };
 
 });
