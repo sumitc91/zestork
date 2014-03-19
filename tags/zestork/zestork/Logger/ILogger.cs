f15f34a92488b4d1cc06861d3a8eb240bc345c2c
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-19 12:22:18 +0000

    log4net added in project

---
+++ tags/zestork/zestork/Logger/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SunPower.Common.Infrastructure.Logger
+{
+   public interface ILogger
+    {
+         void Info(string message);
+         void Error(string message, Exception ex);
+         void Debug(string message, Exception ex);
+         void Fatal(string message, Exception ex);         
+    }
+}
---
+++ tags/zestork/zestork/Logger/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+namespace SunPower.Common.Infrastructure.Logger
+{
+    public class Logger : ILogger
+    {
+        private string _currentClassName;
+        private string _userName;
+        ILog logger = null;
+        public Logger(string currentClassName)
+        {
+            this._currentClassName = currentClassName;
+            logger = LogManager.GetLogger(_currentClassName);
+            BasicConfigurator.Configure();
+            log4net.Config.XmlConfigurator.Configure();
+        }       
+
+        public void Info(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            logger.Error(message, ex);
+        }
+
+        public void Debug(string message, Exception ex)
+        {
+            
+            logger.Debug(message, ex);
+        }
+       
+        public void Fatal(string message, Exception ex)
+        {
+            logger.Fatal(message, ex);
+        }
+       
+
+    }
+}
