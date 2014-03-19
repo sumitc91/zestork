686129e7181a2a38f0e1e3bc5c0d26a750a34d42
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-03-19 12:22:18 +0000

    log4net added in project

--- zestork/Controllers/HomeController.cs
+++ zestork/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SunPower.Common.Infrastructure.Logger;
+using System.Reflection;
 
 namespace zestork.Controllers
 {
@@ -10,9 +12,11 @@ namespace zestork.Controllers
     {
         //
         // GET: /Home/
+        private ILogger logger = new Logger(Convert.ToString(MethodBase.GetCurrentMethod().DeclaringType));
 
         public ActionResult Index()
         {
+            logger.Info("Home-Index");
             return View();
         }
 
