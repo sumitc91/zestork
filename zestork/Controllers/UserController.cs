88cf1fb5f9fe7eee81885062383898dbec0a71e8
d.g.chanchal <dgchanchal@users.noreply.github.com>
2014-03-23 19:35:54 +0000

    after login template added.

---
+++ zestork/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zestork.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index(String id)
+        {
+            string userId = id;            
+
+            return View();
+        }
+
+    }
+}
