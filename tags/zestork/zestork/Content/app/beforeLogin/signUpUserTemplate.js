aa21ede2367605e6a88acd64d0149bba17b7715c
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-26 12:35:44 +0000

    humane.js added for ui messages.

--- tags/zestork/zestork/Content/app/beforeLogin/signUpUserTemplate.js
+++ tags/zestork/zestork/Content/app/beforeLogin/signUpUserTemplate.js
@@ -2,6 +2,8 @@
 
 ZestorkApp.controller('signUpUserController', function ($scope, $http) {
 
+    NProgress.start();
+
     $scope.userName = "";
     $scope.firstName = "";
     $scope.lastName = "";
@@ -21,13 +23,35 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
             type: $scope.type
         }
 
+        //        <option selected="">libnotify</option>
+        //        <option>bigbox</option>
+        //        <option>boldlight</option>
+        //        <option>jackedup</option>
+        //        <option>original</option>
+        //        <option>flatty</option>
+
+        humane.timeout = 30000;
+        humane.baseCls = "humane-boldlight";
+        humane.log("Please wait While Creating your Account....");
+
         $http({
             url: '/Account/CreateAccount',
             method: "POST",
             data: CreateAccountRequest,
             headers: { 'Content-Type': 'application/json' }
         }).success(function (data, status, headers, config) {
-            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here 
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+            humane.remove(function () { });
+            if (data.code == "200") {
+                humane.timeout = 2000;
+                humane.baseCls = "humane-bigbox";
+                humane.log("Account Successfully Created.");
+            }
+            else if (data.code == "402") {
+                humane.timeout = 2000;
+                humane.baseCls = "humane-bigbox";
+                humane.log($scope.userName + "  Already Exists.");
+            }            
             console.log(data);
         }).error(function (data, status, headers, config) {
             $scope.status = status;
@@ -35,6 +59,7 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
 
     }
 
+    NProgress.done();
 });
 
 
