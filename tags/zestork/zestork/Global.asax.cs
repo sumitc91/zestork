755716dd92875f20b6d31e1ac14bc5b8598e9f72
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-27 11:54:57 +0000

    no-cache added in every request.

--- tags/zestork/zestork/Global.asax.cs
+++ tags/zestork/zestork/Global.asax.cs
@@ -20,5 +20,19 @@ namespace zestork
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");            
+            HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");           
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                //HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, tokenId");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
 }
