be4f8802a8928965351f256aa68f5047ac1108fb
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-03-27 14:16:03 +0000

    validation added in signUpUserTemplate.js

--- zestork/Content/app/beforeLogin/signUpUserTemplate.js
+++ zestork/Content/app/beforeLogin/signUpUserTemplate.js
@@ -58,30 +58,33 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
         var checkUserExistsRequestData = {
             userName: $scope.userName
         }
-        $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Checking username availability</h1>' });
+        if (isValidEmailAddress($scope.userName)) {
+            $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Checking username availability</h1>' });
 
-        $http({
-            url: '/Account/checkUsernameExists',
-            method: "POST",
-            data: checkUserExistsRequestData,
-            headers: { 'Content-Type': 'application/json' }
-        }).success(function (data, status, headers, config) {
-            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
-            $.unblockUI();
-            if (data.code == "200") {
-                $.blockUI({ message: '<h1>This is a new username' });
-                setTimeout($.unblockUI, 2000);
-                //alert("Account Successfully Created.");
-            }
-            else if (data.code == "402") {
-                $.blockUI({ message: '<h1>username already exists' });
-                setTimeout($.unblockUI, 2000);
-                //alert("Account already exists.");
-            }
-            console.log(data);
-        }).error(function (data, status, headers, config) {
-            $scope.status = status;
-        });
+            $http({
+                url: '/Account/checkUsernameExists',
+                method: "POST",
+                data: checkUserExistsRequestData,
+                headers: { 'Content-Type': 'application/json' }
+            }).success(function (data, status, headers, config) {
+                //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+                $.unblockUI();
+                if (data.code == "200") {
+                    $.blockUI({ message: '<h1>This is a new username' });
+                    setTimeout($.unblockUI, 2000);
+                    //alert("Account Successfully Created.");
+                }
+                else if (data.code == "402") {
+                    $.blockUI({ message: '<h1>username already exists' });
+                    setTimeout($.unblockUI, 2000);
+                    //alert("Account already exists.");
+                }
+                console.log(data);
+            }).error(function (data, status, headers, config) {
+                $scope.status = status;
+            });
+        }
+        
     }
 
     $scope.submitCreateAccountData = function () {
