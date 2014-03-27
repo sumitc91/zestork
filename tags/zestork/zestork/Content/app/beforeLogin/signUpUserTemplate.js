4ccf50b7b8b6362d914feef49b3741e5921564d5
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-27 10:50:32 +0000

    checking if username already exists implemented.

--- tags/zestork/zestork/Content/app/beforeLogin/signUpUserTemplate.js
+++ tags/zestork/zestork/Content/app/beforeLogin/signUpUserTemplate.js
@@ -5,12 +5,69 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
     NProgress.start();
 
     $scope.userName = "";
+    $scope.emailAlreadyExists = "";
     $scope.firstName = "";
     $scope.lastName = "";
     $scope.password = "";
     $scope.confirmPassword = "";
     $scope.source = "web";
     $scope.type = "user";
+
+    $scope.checkEmailAvailableOnFocusOut = function () {
+        var checkUserExistsRequestData = {
+            userName: $scope.userName
+        }
+
+        $http({
+            url: '/Account/checkUsernameExists',
+            method: "POST",
+            data: checkUserExistsRequestData,
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+
+            if (data.code == "200") {
+                $scope.emailAlreadyExists = "valid email id";
+                //alert("Account Successfully Created.");
+            }
+            else if (data.code == "402") {
+                $scope.emailAlreadyExists = "Email Id Already Exists";
+                //alert("Account already exists.");
+            }
+            console.log(data);
+        }).error(function (data, status, headers, config) {
+            $scope.status = status;
+        });
+    }
+    $scope.checkUserExists = function () {
+        var checkUserExistsRequestData = {
+            userName: $scope.userName
+        }
+        $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Checking username availability</h1>' });
+
+        $http({
+            url: '/Account/checkUsernameExists',
+            method: "POST",
+            data: checkUserExistsRequestData,
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+            $.unblockUI();
+            if (data.code == "200") {
+                $.blockUI({ message: '<h1>This is a new username' });
+                setTimeout($.unblockUI, 2000);
+                //alert("Account Successfully Created.");
+            }
+            else if (data.code == "402") {
+                $.blockUI({ message: '<h1>username already exists' });
+                setTimeout($.unblockUI, 2000);
+                //alert("Account already exists.");
+            }
+            console.log(data);
+        }).error(function (data, status, headers, config) {
+            $scope.status = status;
+        });
+    }
 
     $scope.submitCreateAccountData = function () {
 
@@ -23,7 +80,7 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
             type: $scope.type
         }
 
-        $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Creating your account...</h1>' }); 
+        $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Creating your account...</h1>' });
 
         $http({
             url: '/Account/CreateAccount',
@@ -42,7 +99,7 @@ ZestorkApp.controller('signUpUserController', function ($scope, $http) {
                 $.blockUI({ message: '<h1>Account already exists.' });
                 setTimeout($.unblockUI, 2000);
                 //alert("Account already exists.");
-            }            
+            }
             console.log(data);
         }).error(function (data, status, headers, config) {
             $scope.status = status;
---
+++ tags/zestork/zestork/Models/DataContract/checkUsernameExistsRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zestork.Models.DataContract
+{
+    public class checkUsernameExistsRequest
+    {
+        public string userName { get; set; }
+    }
+}
