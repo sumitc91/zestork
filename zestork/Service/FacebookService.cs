12cee1569105889ef2f13aceaac42c61e86734ac
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-03-28 12:44:53 +0000

    exception logged for FacebookService

--- zestork/Service/FacebookService.cs
+++ zestork/Service/FacebookService.cs
@@ -28,111 +28,121 @@ namespace zestork.Service
 
         private LogOnModel checkFacebookAuthorization(string returnUrl, string code)
         {
-            var _db = new ZestorkContainer();
-            string app_id = string.Empty;
-            string app_secret = string.Empty;
-            if(returnUrl.Contains("zestork.pcongo"))
-            {
-                app_id = ConfigurationManager.AppSettings["FacebookAppIDZestork"].ToString();
-                app_secret = ConfigurationManager.AppSettings["FacebookAppSecretZestork"].ToString(); 
-            }
-            else
-            {
-                app_id = ConfigurationManager.AppSettings["FacebookAppID"].ToString();
-                app_secret = ConfigurationManager.AppSettings["FacebookAppSecret"].ToString(); 
-            }
-            
             var userData = new LogOnModel();
-            string scope = "email";
-            if (code == null)
+            try
             {
-                userData.ReturnUrl = (string.Format(
-                    "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
-                    app_id, returnUrl, scope));
-
-                return userData;
-            }
-            else
-            {
-                Dictionary<string, string> tokens = new Dictionary<string, string>();                
-                string url = string.Format(
-                    "https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
-                    app_id, returnUrl, scope, code, app_secret);
-
-                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                var _db = new ZestorkContainer();
+                string app_id = string.Empty;
+                string app_secret = string.Empty;
+                if (returnUrl.Contains("zestork.pcongo"))
                 {
-
-                    StreamReader reader = new StreamReader(response.GetResponseStream());
-
-                    string vals = reader.ReadToEnd();
-
-                    foreach (string token in vals.Split('&'))
-                    {
-                        tokens.Add(token.Substring(0, token.IndexOf("=")),
-                            token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
-                    }
-
-                }
-
-                string access_token = tokens["access_token"];
-
-                var client = new FacebookClient(access_token);
-                dynamic me = client.Get("me");                           
-                String userName = Convert.ToString(me.username);
-                userData.User = new User();
-                if (_db.Users.Any(x => x.Username == userName + "@facebook.com"))
-                {
-                    Users user = _db.Users.SingleOrDefault(x => x.Username == userName + "@facebook.com");
-                    //user is already registered
-                    userData.User.FirstName = user.FirstName;
-                    userData.User.LastName = user.LastName;
-                    userData.User.Username = user.Username;
-                    userData.User.Gender = user.gender;
-                    userData.User.ImageUrl = user.ImageUrl;
+                    app_id = ConfigurationManager.AppSettings["FacebookAppIDZestork"].ToString();
+                    app_secret = ConfigurationManager.AppSettings["FacebookAppSecretZestork"].ToString();
                 }
                 else
                 {
-                    // add user in database
-                    String ID = Guid.NewGuid().ToString();
-                    String ImageUrl = FacebookService.GetPictureUrl(userName);
-
-                    userData.User.FirstName = me.first_name;
-                    userData.User.LastName = me.last_name;
-                    userData.User.Username = me.username;
-                    userData.User.Email = me.email;
-                    userData.User.Gender = me.gender;
-                    userData.User.ImageUrl = ImageUrl;
-                    var user = new Users
-                    {
-                        Username = userName + "@facebook.com",
-                        Password = Guid.NewGuid().ToString(),
-                        Source = "facebook",
-                        isActive = "true",
-                        Type = "user",
-                        guid = "abcde",
-                        FirstName = me.first_name,
-                        LastName = me.last_name,
-                        gender = me.gender,
-                        ImageUrl = ImageUrl
-                    };
-
-                    _db.Users.Add(user);
-
-                    try
-                    {
-                        _db.SaveChanges();                        
-                    }
-                    catch (DbEntityValidationException e)
-                    {
-                        dbContextException dbContextException = new CommonMethods.dbContextException();
-                        dbContextException.logDbContextException(e);
-                        throw;
-                    }
+                    app_id = ConfigurationManager.AppSettings["FacebookAppID"].ToString();
+                    app_secret = ConfigurationManager.AppSettings["FacebookAppSecret"].ToString();
                 }
-                
+
+               
+                string scope = "email";
+                if (code == null)
+                {
+                    userData.ReturnUrl = (string.Format(
+                        "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
+                        app_id, returnUrl, scope));
+
+                    return userData;
+                }
+                else
+                {
+                    Dictionary<string, string> tokens = new Dictionary<string, string>();
+                    string url = string.Format(
+                        "https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+                        app_id, returnUrl, scope, code, app_secret);
+
+                    HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+
+                        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                        string vals = reader.ReadToEnd();
+
+                        foreach (string token in vals.Split('&'))
+                        {
+                            tokens.Add(token.Substring(0, token.IndexOf("=")),
+                                token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                        }
+
+                    }
+
+                    string access_token = tokens["access_token"];
+
+                    var client = new FacebookClient(access_token);
+                    dynamic me = client.Get("me");
+                    String userName = Convert.ToString(me.username);
+                    userData.User = new User();
+                    if (_db.Users.Any(x => x.Username == userName + "@facebook.com"))
+                    {
+                        Users user = _db.Users.SingleOrDefault(x => x.Username == userName + "@facebook.com");
+                        //user is already registered
+                        userData.User.FirstName = user.FirstName;
+                        userData.User.LastName = user.LastName;
+                        userData.User.Username = user.Username;
+                        userData.User.Gender = user.gender;
+                        userData.User.ImageUrl = user.ImageUrl;
+                    }
+                    else
+                    {
+                        // add user in database
+                        String ID = Guid.NewGuid().ToString();
+                        String ImageUrl = FacebookService.GetPictureUrl(userName);
+
+                        userData.User.FirstName = me.first_name;
+                        userData.User.LastName = me.last_name;
+                        userData.User.Username = me.username;
+                        userData.User.Email = me.email;
+                        userData.User.Gender = me.gender;
+                        userData.User.ImageUrl = ImageUrl;
+                        var user = new Users
+                        {
+                            Username = userName + "@facebook.com",
+                            Password = Guid.NewGuid().ToString(),
+                            Source = "facebook",
+                            isActive = "true",
+                            Type = "user",
+                            guid = "abcde",
+                            FirstName = me.first_name,
+                            LastName = me.last_name,
+                            gender = me.gender,
+                            ImageUrl = ImageUrl
+                        };
+
+                        _db.Users.Add(user);
+
+                        try
+                        {
+                            _db.SaveChanges();
+                        }
+                        catch (DbEntityValidationException e)
+                        {
+                            dbContextException dbContextException = new CommonMethods.dbContextException();
+                            dbContextException.logDbContextException(e);
+                            throw;
+                        }
+                    }
+
+                }
             }
+            catch (Exception ex)
+            {
+                logger.Error("facebook login error", ex);
+                throw;
+            }
+            
 
             return userData;
         }
