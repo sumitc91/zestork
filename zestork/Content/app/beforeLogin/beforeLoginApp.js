0210e840310c448808eb1917dfe01016cd92db2e
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-03-28 12:12:09 +0000

    facebookLogin.html added in project.

--- zestork/Content/app/beforeLogin/beforeLoginApp.js
+++ zestork/Content/app/beforeLogin/beforeLoginApp.js
@@ -5,6 +5,7 @@ ZestorkApp.config(function ($routeProvider) {
 
     $routeProvider.when("/", { templateUrl: "../../Resource/templates/beforeLogin/contentView/home.html" }).
                    when("/signup/user", { templateUrl: "../../Resource/templates/beforeLogin/contentView/signupuser.html" }).
+                   when("/facebookLogin", { templateUrl: "../../Resource/templates/beforeLogin/contentView/facebookLogin.html" }).
                    when("/validate/:userName/:guid", { templateUrl: "../../Resource/templates/beforeLogin/contentView/validateAccount.html" }).
                    when("/about", { templateUrl: "../../Resource/templates/beforeLogin/contentView/about.html" }).
                    when("/404", { templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" }).
