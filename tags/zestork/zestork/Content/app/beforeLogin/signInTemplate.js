7a512c28732c787630d91810f073b937a39460d9
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-03-28 12:12:09 +0000

    facebookLogin.html added in project.

--- tags/zestork/zestork/Content/app/beforeLogin/signInTemplate.js
+++ tags/zestork/zestork/Content/app/beforeLogin/signInTemplate.js
@@ -1,10 +1,32 @@
-ZestorkApp.controller('signInController', function ($scope) {
+ZestorkApp.controller('signInController', function ($scope,$http) {
     $scope.data = "this is my testing data sign in page";
     $scope.signInUsingFacebook = function () {
         alert("working");
-
     }
     $scope.test = function () {
         alert('1');
     };
+});
+
+ZestorkApp.controller('facebookLoginController', function ($scope,$http) {
+    $.blockUI({ message: '<h1><img src="../../Content/third-party/bootstrap-modal-master/img/ajax-loader.gif" /> Loggin in via Facebook..</h1>' });
+
+    $http({
+        url: '/Account/Login',
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+    }).success(function (data, status, headers, config) {
+        //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+        $.unblockUI();
+        if (data.User == null) {
+            window.location.href = data.ReturnUrl;
+        }
+        else {
+            alert("successfully Logged in using facebook");
+            console.log(data);
+        }
+        console.log(data);
+    }).error(function (data, status, headers, config) {
+        $scope.status = status;
+    });
 });
