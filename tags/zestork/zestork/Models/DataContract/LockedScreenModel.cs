776e3a49c1be7fb7da0ed903ea3745637743faec
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-04-07 17:29:35 +0000

    Locked screen unlocking implemented. model file added

---
+++ tags/zestork/zestork/Models/DataContract/LockedScreenModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zestork.Models.DataContract
+{
+    public class LockedScreenModel
+    {
+        public string imageUrl { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string message { get; set; }
+        public string guid { get; set; }
+        public string postUrl { get; set; }
+    }
+}
