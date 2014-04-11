f8fff8269299deecac999839e6b85f7863df41b6
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-04-11 12:32:45 +0000

    image upload service added in the project.

---
+++ tags/zestork/zestork/Models/DataWrapper/imgurUploadImageResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zestork.Models.DataWrapper
+{
+    public class imgurUploadImageResponse
+    {
+        public data data { get; set; }
+    }
+    public class data
+    {
+        public string deletehash { get; set; }
+        public string link { get; set; }
+    }
+}
