5c1589c6637386f1dbe1880df4fc6f821ac7909c
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-04-16 09:48:01 +0000

    Encryption class added but not implemented yet.

--- zestork/Content/app/beforeLogin/signInTemplate.js
+++ zestork/Content/app/beforeLogin/signInTemplate.js
@@ -103,6 +103,6 @@ ZestorkApp.controller('LoginViewController', function ($scope, $routeParams) {
         $scope.content = "either username or password is wrong ! try again.";
     }
 
-    $scope.test = "testing";
+    $scope.signedInModel = "false";
     
 });
