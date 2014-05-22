9876ef55a74114b1d8feeb8d14972aaf405f47ff
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-05-22 09:38:06 +0000

    createProductSurvey.html edit. instruction list dynamically increamenting.

--- zestork/Content/app/clientView/createProductSurveyTemplate.js
+++ zestork/Content/app/clientView/createProductSurveyTemplate.js
@@ -1,17 +1,15 @@
 //getting user info..
 ZestorkAppClientView.controller('createProductSurveyController', function ($scope, $http, $rootScope, $routeParams, CookieUtil) {
-    //alert("create product controller");
+    //alert("create product controller");    
     var editableInstructions = "";
     var totalEditableInstruction = 1;
-    editableInstructions += "<li>";
-    editableInstructions += "<a data-original-title='Enter Instruction #1' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction1' class='editable editable-click editable-empty editableInstruction'>Instructions 1</a>";
-    editableInstructions += "</li>";
-    //    editableInstructions += "<li>";
-    //    editableInstructions += "<a data-original-title='Enter Instruction #1' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction2' class='editable editable-click editable-empty editableInstruction'>Instructions 2</a>";
-    //    editableInstructions += "</li>";
-    //    editableInstructions += "<li>";
-    //    editableInstructions += "<a data-original-title='Enter Instruction #1' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction3' class='editable editable-click editable-empty editableInstruction'>Instructions 3</a>";
-    //    editableInstructions += "</li>";
+    $scope.editableInstructionsList = [{ Number: 1, Text: "Instruction 1"}];
+
+    $.each($scope.editableInstructionsList, function () {
+        editableInstructions += "<li>";
+        editableInstructions += "<a data-original-title='Enter Instruction #" + this.Number + "' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction" + this.Number + "' class='editable editable-click editable-empty editableInstruction'>" + this.Text + "</a>";
+        editableInstructions += "</li>";
+    });
 
     $('#editableInstructionsListID').html(editableInstructions);
 
@@ -22,11 +20,16 @@ ZestorkAppClientView.controller('createProductSurveyController', function ($scop
         }
     });
 
-    $scope.addEditableInstructions = function (index) {    
-        totalEditableInstruction++;
-        editableInstructions += "<li>";
-        editableInstructions += "<a data-original-title='Enter Instruction #1' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction" + totalEditableInstruction + "' class='editable editable-click editable-empty editableInstruction'>Instructions " + totalEditableInstruction + "</a>";
-        editableInstructions += "</li>";
+    $scope.addEditableInstructions = function (index) {
+        totalEditableInstruction = totalEditableInstruction + 1;
+        var editableInstructionDataToBeAdded = { Number: totalEditableInstruction, Text: "Instruction " + totalEditableInstruction };
+        $scope.editableInstructionsList.push(editableInstructionDataToBeAdded);
+        editableInstructions = "";
+        $.each($scope.editableInstructionsList, function () {
+            editableInstructions += "<li>";
+            editableInstructions += "<a data-original-title='Enter Instruction #" + this.Number + "' data-placeholder='Required' data-placement='right' data-pk='1' data-type='text'  href='#' id='clientEditableInstruction" + this.Number + "' class='editable editable-click editable-empty editableInstruction'>" + this.Text + "</a>";
+            editableInstructions += "</li>";
+        });
         $('#editableInstructionsListID').html(editableInstructions);
 
         //editables 
@@ -37,3 +40,4 @@ ZestorkAppClientView.controller('createProductSurveyController', function ($scop
         });
     }
 });
+
